2dca5398fab05dc21ffacfbc4a8319c3bf5833a5
Alexandra Roth <alroth8@gmail.com>
2017-02-21 10:42:01 -0500

    Done.

---
+++ app/models/super_heros.rb
@@ -0,0 +1,20 @@
+class SuperHeros
+
+	attr_accessor :name, :power, :bio
+
+
+ALL_HEROS = []
+
+	def initialize(name, power, bio)
+		@name = name
+		@power = power
+		@bio = bio
+		ALL_HEROS << self
+	end
+
+	def self.all 
+		ALL_HEROS
+	end
+	
+	
+end
